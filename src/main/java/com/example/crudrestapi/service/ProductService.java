9f09522b58992c04f3845432b816a9498592b7e8
Nzovia <nickiemaundu19@gmail.com>
2022-01-14 13:45:58 +0300

    Updated the service class and added all the CRUD operations necessary

--- src/main/java/com/example/crudrestapi/service/ProductService.java
+++ src/main/java/com/example/crudrestapi/service/ProductService.java
@@ -1,12 +1,36 @@
 package com.example.crudrestapi.service;
 
 import com.example.crudrestapi.Repository.ProductRepository;
+import com.example.crudrestapi.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 //service class stands at the mid of persistent layer and the data access layer
 public class ProductService {
     @Autowired
     private ProductRepository productRepo;
+
+    //first method to list all the products
+    public List<Product> displayAll(){
+        return productRepo.findAll();
+    }
+
+    //product saving method
+    public void save(Product productData){
+        productRepo.save(productData);
+    }
+
+    //getting a particular product using it's ID
+    public  Product get(Long id){
+        //delegate a call to the repo
+        return productRepo.findById(id).get();
+    }
+
+    //delete specific product by its own id
+    public void delete(Long id){
+        productRepo.deleteById(id);
+    }
 }
